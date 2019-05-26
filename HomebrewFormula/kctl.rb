0b94a865f875ef081ca2c6ce7a1179192df58c62
Arvind Thangamani <dnivra26@gmail.com>
2019-05-26 21:23:15 +0530

    update brew formula

--- HomebrewFormula/kctl.rb
+++ HomebrewFormula/kctl.rb
@@ -2,7 +2,7 @@ class Kctl < Formula
   desc "A CLI wrapper for making kubernetes commands much easier"
   homepage "https://github.com/alaminopu/kctl"
   url "https://github.com/alaminopu/kctl/releases/download/0.2.1/kctl-0.2.1-x86_64-darwin.tar.gz"
-  sha256 "5bd5558d2488023ab3f55a77606de1d75305e7a1cf7c0e2a321026e7ca66e781"
+  sha256 "5e065d08753f50b5ebefb6cbde37f7abf0ac0215ba1959602f8d5343ff8e806a"
   version "0.2.1"
   def install
     bin.install "kctl"
