4ef81102ec6cb78ad2a155db5cdd786627f494c9
Arvind Thangamani <dnivra26@gmail.com>
2019-05-26 21:10:36 +0530

    add version to brew formula

--- HomebrewFormula/kctl.rb
+++ HomebrewFormula/kctl.rb
@@ -3,7 +3,7 @@ class Kctl < Formula
   homepage "https://github.com/alaminopu/kctl"
   url "https://github.com/alaminopu/kctl/releases/download/0.2.1/kctl-0.2.1-x86_64-darwin.tar.gz"
   sha256 "5bd5558d2488023ab3f55a77606de1d75305e7a1cf7c0e2a321026e7ca66e781"
-
+  version "0.2.1"
   def install
     bin.install "kctl"
   end
